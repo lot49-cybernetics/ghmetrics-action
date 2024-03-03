5d8ab78e5e7ce85fce0c9b36f2f95cd3b6b860b8
0xG0nz0 <8682922+0xg0nz0@users.noreply.github.com>
2024-03-03 19:51:00 +0000

    Fix unit tests; fix linting errors

--- src/install_repostat.ts
+++ src/install_repostat.ts
@@ -1,38 +1,42 @@
-import { exec } from 'child_process';
+import { exec } from 'child_process'
 
 /**
  * Install repostat from its Git repository.
  * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
  */
-export async function install(version: string = 'latest', source: string = 'pypi'): Promise<string> {
-  return new Promise(resolve => {
-    let pip_cmd: string = "pip3 install --user --quiet"
-    let install_cmd: string;
+export async function install(
+  version = 'latest',
+  source = 'pypi'
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const pip_cmd = 'pip3 install --user --quiet'
+    let install_cmd: string
     if (source === 'github') {
-      let version_tag: string = '';
+      let version_tag = ''
       if (version !== 'latest') {
-        version_tag = `@${version}`;
+        version_tag = `@${version}`
       }
-      install_cmd = `${pip_cmd} git+https://github.com/vifactor/repostat${version_tag}`;
+      install_cmd = `${pip_cmd} git+https://github.com/vifactor/repostat${version_tag}`
     } else if (source === 'pypi') {
-      install_cmd = `${pip_cmd} repostat${version !== 'latest' ? '==' + version : ''}`;
+      install_cmd = `${pip_cmd} repostat${version !== 'latest' ? '==' + version : ''}`
     } else {
       throw new Error('Invalid source')
     }
     exec(install_cmd, (error, stdout, stderr) => {
       if (error) {
-          console.error(`Error installing package: ${error.message}`);
-          return;
+        reject(`Error installing package: ${error.message}`)
+        return
       }
       if (stderr) {
-          console.error(`Error installing package: ${stderr}`);
-          return;
+        reject(`Error installing package: ${stderr}`)
+        return
       }
       if (stdout) {
-        console.warn(`Package installed (unexpected output in quiet mode): ${stdout}`);
-      } else {
-        console.log(`Package installed successfully`);
+        console.warn(
+          `Package installed (unexpected output in quiet mode): ${stdout}`
+        )
       }
-  });
+      resolve(`Package installed successfully`)
+    })
   })
 }
